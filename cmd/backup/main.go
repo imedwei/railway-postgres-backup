92d21a324893a9e6b1e90cd62c3cdc96337a6d22
Ed Wei <ed@edwei.com>
2025-07-20 20:41:10 -0700

    Initialize Go module and basic entry point
    - Create go.mod for github.com/imedwei/railway-postgres-backup
    - Add placeholder main.go with basic structure

---
+++ cmd/backup/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	log.Println("Railway PostgreSQL Backup Service starting...")
+
+	// TODO: Load configuration
+	// TODO: Initialize storage provider
+	// TODO: Check respawn protection
+	// TODO: Perform backup
+	// TODO: Handle cleanup
+
+	fmt.Println("Backup process not yet implemented")
+	os.Exit(0)
+}
